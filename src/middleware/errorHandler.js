ee8d38d07406c4b11a95542e9826e604b363f1ce
Svitlana Shumal <svetlankachajka@gmail.com>
2025-10-06 23:37:54 +0300

    Added  01 task

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,11 @@
+export const errorHandler = (err, req, res, _next) => {
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
+};

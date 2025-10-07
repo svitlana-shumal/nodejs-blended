23e2e6e6313f4883e9897cac42e5fe36f947c9dc
Svitlana Shumal <svetlankachajka@gmail.com>
2025-10-07 14:02:42 +0300

    Added 03-task

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,1 +1,8 @@
+import { Router } from 'express';
+import { getAllProducts } from '../controllers/productsController.js';
 
+const router = Router();
+
+router.get('/products', getAllProducts);
+
+export default router;

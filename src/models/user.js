07d39bf1b58154147da77916336d6921eaec4279
Svitlana Shumal <svetlankachajka@gmail.com>
2025-10-07 15:06:51 +0300

    Added 01 task at 2 blended

---
+++ src/models/user.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+userSchema.pre('save', function (next) {
+  if (!this.username) {
+    this.username = this.email;
+  }
+  next();
+});
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const User = model('User', userSchema);
